b3af42c16b3f4968f1c7a73d3f253eda6da52cd8
Bart <bart.rybak@hotmail.com>
2022-10-31 17:31:05 +0000

    implemented DB Migrate code on startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,8 +11,7 @@ namespace API
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
-            _config = config;
-            
+            _config = config;            
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
