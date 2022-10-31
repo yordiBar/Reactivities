2570140085eddc1f0f9742672f00ac7819f1a644
Bart <bart.rybak@hotmail.com>
2022-10-31 22:37:33 +0000

    added Semantic-UI package

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
 
@@ -15,17 +16,17 @@ function App() {
   })
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {activities.map((activity: any) => (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+        
+        <List>
+        {activities.map((activity: any) => (
             <li key={activity.id}>
               {activity.title}
             </li>
           ))}
-        </ul>
-      </header>
+        </List>
+          
     </div>
   );
 }
